054ea55315b2c2228b2b41a8c4ff3238faf59af4
Андрей <amore1302@mail.ru>
2024-04-22 20:58:24 +0300

    create bd

---
+++ bd/restorans.py
@@ -0,0 +1,38 @@
+"""
+restaurants  таблица ресторанов
+Поля
+user_id  id пользователя
+name название
+address адрес
+
+
+CREATE TABLE restaurants (
+    restaurant_id INTEGER PRIMARY KEY  AUTOINCREMENT,
+    name TEXT   NOT NULL,
+    address TEXT  NOT NULL
+);
+
+"""
+
+import sqlite3
+
+# Подключаемся к базе данных (или создаем новую)
+conn = sqlite3.connect('../samo_mag_bot.db')
+cursor = conn.cursor()
+
+# Создаем таблицу, если она еще не существует
+cursor.execute('''
+CREATE TABLE restaurants (
+    restaurant_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT  NOT NULL,
+    address TEXT  NOT NULL
+);
+''')
+
+
+
+# Сохраняем изменения в базе данных и закрываем соединение
+conn.commit()
+conn.close()
+
+
