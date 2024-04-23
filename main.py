226622ba6b8fc47c1d03cb9bff0abf3426f6d1da
Olga-Pro <oip_@inbox.ru>
2024-04-23 14:28:31 +0300

    replace  @bot.message_handler(commands=["start"])

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def  sql_list_command(message):
     curent_pos = 0
     res_str = 'Список ресторанов :\n'
     for row in rows:
-        curent_pos = curent_pos + 1;
+        curent_pos = curent_pos + 1
         res_str = res_str + f'{curent_pos} {row[1]}\n'
     if curent_pos <= 0:
         res_str = 'Список ресторанов пустой !!!'
@@ -48,9 +48,35 @@ TOKEN = '7093861093:AAH5k8q2J-fITlb7lfSOv3hlYFtwLCG1CsM'
 bot = telebot.TeleBot(TOKEN)
 
 
-@bot.message_handler(commands=["start"])
-def start_message(message):
-    bot.reply_to(message, 'Привет! Я чат бот Доставка еды !')
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    # Подключение к базе данных SQLite
+    conn = sqlite3.connect('./samo_mag_bot.db', check_same_thread=False)
+    cursor = conn.cursor()
+
+    user_first_name = message.from_user.first_name  # Имя пользователя
+    user_username = '@'+message.from_user.username  # Ник пользователя в Telegram (может быть None)
+    user_id = message.from_user.id
+
+    cursor.execute('SELECT * FROM users WHERE telega_id=?', (user_id,))
+    user = cursor.fetchone()
+
+    if user:
+        bot.send_message(message.chat.id, f"Добро пожаловать, {user_first_name}! Я чат-бот Доставка еды!")
+    else:
+        cursor.execute('INSERT INTO users (telega_id, telega_url, name) VALUES (?, ?, ?)',
+                       (user_id, user_username, user_first_name))
+        conn.commit()
+
+        cursor.execute('SELECT * FROM users WHERE telega_id=?', (user_id,))
+        user = cursor.fetchone()
+
+    conn.close()
+
+    return user[0]
+# @bot.message_handler(commands=["start"])
+# def start_message(message):
+#     bot.reply_to(message, 'Привет! Я чат бот Доставка еды !')
 
 @bot.message_handler(commands=["list"])
 def list_message(message):
